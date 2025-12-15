8121aaa9bfd8f2bd87376c0325054042087597b0
SergeevSergey17484 <podgdmxmdhdmdgs@gmail.com>
2025-12-15 18:02:49 +0300

    Allow editing user role and optional password on update

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -20,7 +20,13 @@ $User = Config::getObject('core.user.class');
     <h5>Введите имя пользователя</h5>
     <input type="text" name="login" placeholder="логин пользователя" value="<?= $viewAdminusers->login ?>"><br>
     <h5>Введите пароль</h5>
-    <input type="text" name="pass" placeholder="новый пароль" value=""><br>
+    <input type="password" name="pass" placeholder="новый пароль (оставьте пустым, чтобы не менять)" value=""><br>
+    <small>Оставьте поле пустым, если не хотите менять пароль</small><br>
+    <h5>Права доступа</h5>
+    <select name="role" id="role">
+        <option value="admin" <?= ($viewAdminusers->role == 'admin') ? 'selected' : '' ?>>Администратор</option>
+        <option value="auth_user" <?= ($viewAdminusers->role == 'auth_user') ? 'selected' : '' ?>>Зарегистрированный пользователь</option>
+    </select><br>
     <h5>Введите e-mail</h5>
     <input type="text" name="email"  placeholder="email" value="<?= $viewAdminusers->email ?>"><br>
     
